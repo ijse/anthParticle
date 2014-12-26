5cad0e00e4a9d673c647e0ff436fe63ca74ec482
ijse <i@ijser.cn>
2014-12-26 10:49:39 +0800

    update gulpfile, add uglify in build'

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@ var should = require('should');
 
 var runSequence = require('run-sequence');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var restartCount = 0;
 
 gulp.task('jshint', function() {
   return gulp.src([
@@ -23,8 +27,24 @@ gulp.task('test', function() {
   return gulp.src('test/test.js', { read: false })
     .pipe(mocha({
         timeout: 5000,
-        reporter: 'spec'
-    }));
+        reporter: 'spec',
+        globals: {
+          should: should
+        }
+    }))
+    .on('error', gutil.log);
+});
+
+gulp.task('release', function() {
+  // todo: browserify, uglify, copy, ...
+  gulp.src('index.js')
+    .pipe(browserify({
+      insertGlobals: false,
+      debug: false
+    }))
+    .pipe(rename('anthParticle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/release'));
 });
 
 gulp.task('build', function() {
@@ -38,13 +58,26 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./build/'));
 });
 
+
+gulp.task('restartCount', function() {
+  restartCount ++;
+
+  var st = '';
+  st += '//////////////////////////////////////////\n';
+  st += '/// No.' + restartCount + ' restart.\n';
+  st += '//////////////////////////////////////////';
+
+  gutil.log('\n' + st);
+
+});
+
 gulp.task('watch', function() {
   gulp.watch([
     'lib/**', 'test/**'
-  ], ['jshint', 'test']);
+  ], ['restartCount', 'jshint', 'test', 'build']);
 });
 
 gulp.task('default', function() {
   // start
-  return runSequence('jshint', 'test');
+  return runSequence('watch');
 });
