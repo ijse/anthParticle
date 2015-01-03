6e48ee6c7596227b5a15ca60268e839a983684d3
ijse <i@ijser.cn>
2015-01-03 15:04:41 +0800

    add scene

--- test/test-main.js
+++ test/test-main.js
@@ -3,9 +3,17 @@ var should = chai.should();
 var assert = chai.assert;
 var expect = chai.expect;
 
+var fs = require('fs');
+var Scene = require('../lib/scene.js');
+
 var anthParticle = require('../lib/index.js');
 
 describe('Test main', function(){
+
+  var configXml = fs.readFileSync(__dirname + '/res/snow/config.xml').toString();
+  var sceneData = require('./res/snow/config.json').scene;
+  var sceneImage = new Image();
+  sceneImage.src = 'data:image/png;base64,' + fs.readFileSync(__dirname + '/res/snow/cypic.png').toString('base64');
 
   it('check function', function() {
 
@@ -35,6 +43,18 @@ describe('Test main', function(){
       // console.log(counter);
       done();
     }, 1200);
+  });
+
+  it('create and init one scene', function(done) {
+    var ins = new anthParticle({
+      fps: 10,
+      canvas: document.createElement('canvas')
+    });
+    ins.load(configXml, sceneImage)
+      .then(function() {
+        expect(ins.curScene instanceof Scene).to.be.true;
+        done();
+      }).fail(done);
   });
 
 });
