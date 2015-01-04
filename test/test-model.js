13786aff0f2022d5d7e4dd369c9ac6889271b3be
ijse <i@ijser.cn>
2015-01-04 12:36:51 +0800

    append tests

--- test/test-model.js
+++ test/test-model.js
@@ -6,20 +6,28 @@ var expect = chai.expect;
 var fs = require('fs');
 var Model = require('../lib/model.js');
 var Particle = require('../lib/particle.js');
+var Vector = require('../lib/vector.js');
 var util = require('../lib/util.js');
 
 describe('Test model', function() {
     "use strict";
+    var model = null;
     var modelData = require('./res/snow/config.json').scene.model[0];
     var modelImage = new Image();
     modelImage.src = 'data:image/png;base64,' + fs.readFileSync(__dirname + '/res/snow/cypic.png').toString('base64');
 
     it('should get an instance of Model', function() {
-        var model = new Model(modelData, function(ltwh) {
+        model = new Model(modelData, function(ltwh) {
             return util.clipImageToCanvas.bind(null, modelImage).apply(null, ltwh);
         });
 
         expect(model instanceof Model).to.be.true;
         expect(model instanceof Particle).to.be.true;
+    });
+
+    it('should have some initial attributes', function() {
+        expect(model.life).to.be.above(0);
+        expect(model.position instanceof Vector).to.be.true;
+        expect(model.velocity instanceof Vector).to.be.true;
     });
 });
