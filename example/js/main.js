30442a5ee72205ae5dbbcc06dbd51f3cc4cf764d
ijse <i@ijser.cn>
2015-01-08 20:36:36 +0800

    add xml editor to example

--- example/js/main.js
+++ example/js/main.js
@@ -3,16 +3,14 @@ $(function() {
   if(typeof debug !== 'undefined') {
     debug.enable('ap:*');
   }
+  var xmlEditorDom = document.getElementById('xmlEditor');
 
-  var myCanvas = document.getElementById('my');
-  // myCanvas.width = 800;
-  // myCanvas.height = 600;
-  var myCtx = myCanvas.getContext('2d');
-
-  var particle = new anthParticle({
-    fps: 60,
-    canvas: myCanvas
+  var xmlEditor = CodeMirror.fromTextArea(xmlEditorDom, {
+    lineNumbers: true,
+    theme: 'night',
+    mode: 'xml'
   });
+
 
   // get xml
   $.ajax({
@@ -20,29 +18,63 @@ $(function() {
     url: './data/config.xml',
     dataType: 'text'
   }).success(function(data) {
-    // get picture
-    var resImg = new Image();
-    resImg.src = './data/cypic.png?' + (+new Date());
-    resImg.onload = function() {
 
-      // create particle
-      particle
-      .load(data, this)
-      .then(function() {
-        particle.start();
-
-        setTimeout(function() {
-          console.log('stop!');
-          particle.stop();
-        }, 50000);
-
-      }).fail(function(e) {
-        // console.log(e);
-        throw e;
-      });
-    };
-
+    xmlEditor.setValue(data);
   });
 
+
+  var app = (function() {
+    var myCanvas = document.getElementById('my');
+    // myCanvas.width = 800;
+    // myCanvas.height = 600;
+    var myCtx = myCanvas.getContext('2d');
+
+    var particle = new anthParticle({
+      fps: 60,
+      canvas: myCanvas
+    });
+
+    return {
+      stopEmulator: function() {
+        $('#stopBtn').hide();
+        $('#startBtn').show();
+        particle.stop();
+      },
+      startEmulator: function() {
+
+        $('#startBtn').hide();
+        $('#stopBtn').show();
+
+        var xmlData = xmlEditor.getValue();
+        var imgData = document.getElementById('imgRes');
+
+        particle.stop();
+
+        particle
+        .load(xmlData, imgData)
+        .then(function() {
+          particle.start();
+
+          // setTimeout(function() {
+          //   console.log('stop!');
+          //   particle.stop();
+          // }, 50000);
+
+        }).fail(function(e) {
+          // console.log(e);
+          throw e;
+        });
+
+      },
+      setImage: function(img) {
+        return this.imgData = img;
+      },
+      setXml: function(xml) {
+        return this.xmlData = xml;
+      }
+    }
+  })();
+
+  window.app = app;
 
 });
